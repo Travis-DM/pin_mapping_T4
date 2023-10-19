176849ba1f6d2015119f41ba9d03a6925446fc27
TCP-ENG <129853844+Travis-DM@users.noreply.github.com>
2023-10-19 10:34:57 -0400

    Initial Commit

---
+++ pin_mapping_T4.cpp
@@ -0,0 +1,350 @@
+#include <Arduino.h>
+//#include "pin_config.h"
+#include "teensy_map.h"
+#include "pin_setting.h"
+#include "can_interface.h"
+#include "analog.h"
+#include "gpio.h"
+#include "pwm.h"
+
+bool busart0EN;
+bool busart1EN;
+bool busart2EN;
+bool busart3EN;
+bool busart4EN;
+bool busart5EN;
+bool busart6EN;
+bool busart7EN;
+
+
+bool bcan0EN;
+bool bcan1EN;
+bool bcan2EN;
+
+bool bled0EN;
+bool bled1EN;
+bool bled2EN;
+bool bled3EN;
+
+bool bi2c0EN;
+bool bi2c1EN;
+bool bi2c2EN;
+
+void parsePins(u_int8_t *pinCon)
+{
+    parsePins(pinCon, 42);
+}
+
+
+void parsePins(u_int8_t *pinCon, int pinCount){
+    busart0EN = false;
+    busart1EN = false;
+    busart2EN = false;
+    busart3EN = false;
+    busart4EN = false;
+    busart5EN = false;
+    busart6EN = false;
+    busart7EN = false;
+
+    bcan0EN = false;
+    bcan1EN = false;
+    bcan2EN = false;
+
+    bled0EN = false;
+    bled1EN = false;
+    bled2EN = false;
+    bled3EN = false;
+
+    bi2c0EN = false;
+    bi2c1EN = false;
+    bi2c2EN = false;
+
+    gpoCount = 0;
+    gpiCount = 0;
+    gpioCount = 0;
+    adcCount = 0;
+    pwmCount = 0;
+
+
+    for(int x = 0; x < pinCount;x++)
+    {
+        gpio[x].SetPin(x);
+        switch (pinCon[x])
+        {
+        case _GPO:
+            gpio[x].Setup(x,false);
+            gpoCount++;
+            gpioCount++;
+            debug.printf("Pin: %d is Set as digital output\n",x);
+            break;
+        case _GPI:
+            gpio[x].Setup(x,true);
+            gpiCount++;
+            gpioCount++;
+            debug.printf("Pin: %d is Set as digital input\n",x);
+            break;
+        case _CAN:
+            debug.printf("Pin: %d is Set as CAN\n",x);
+            switch (x)
+            {
+                case 0:
+                case 1:
+                    bcan1EN = true;
+                    debug.println("CAN1 Enabled");
+                    break;
+                case 22:
+                case 23:
+                    bcan0EN = true;
+                    debug.println("CAN0 Enabled");
+                    break;
+                case 30:
+                case 31:
+                    bcan2EN = true;
+                    debug.println("CAN2 Enabled");
+                    break;
+            }
+            break;
+        case _LEDM0:
+            debug.printf("Pin: %d is Set as LED\n",x);
+            
+            pinMode(x,OUTPUT);
+            bled0EN = true;
+            break;
+        case _USART:
+            debug.printf("Pin: %d is Set as USART\n",x);
+            switch (x)
+            {
+                case 0:
+                case 1:
+                    //read config
+                    USART0.begin(115200);
+                    busart0EN = true;
+                    debug.println("USART0 Enabled");
+                    break;
+                case 7:
+                case 8:
+                    USART1.begin(115200);
+                    busart1EN = true;
+                    debug.println("USART1 Enabled");
+                    break;
+                case 14:
+                case 15:
+                    USART2.begin(115200);
+                    busart2EN = true;
+                    debug.println("USART2 Enabled");
+                    break;
+                case 16:
+                case 17:
+                    USART3.begin(115200);
+                    busart3EN = true;
+                    debug.println("USART3 Enabled");
+                    break;
+                case 20:
+                case 21:
+                    USART4.begin(115200);
+                    busart4EN = true;
+                    debug.println("USART4 Enabled");
+                    break;
+                case 24:
+                case 25:
+                    //USART0.begin(115200);
+                    //usart0EN = true;
+                    debug.println("Error USART5 Is USED for I2C");
+                    break;
+                case 28:
+                case 29:
+                    USART6.begin(115200);
+                    busart6EN = true;
+                    debug.println("USART6 Enabled");
+                    break;
+                case 34:
+                case 35:
+                    USART7.begin(115200);
+                    busart7EN = true;
+                    debug.println("USART7 Enabled");
+                    break;  
+            }
+            break;
+            case _LIN:
+            debug.printf("Pin: %d is Set as LIN\n",x);
+            switch (x)
+            {
+                case 0:
+                case 1:
+                    USART0.begin(115200);
+                    busart0EN = true;
+                    debug.println("LIN0 Enabled");
+                    break;
+                case 7:
+                case 8:
+                    USART1.begin(115200);
+                    busart1EN = true;
+                    debug.println("LIN1 Enabled");
+                    break;
+                case 14:
+                case 15:
+                    USART2.begin(115200);
+                    busart2EN = true;
+                    debug.println("LIN2 Enabled");
+                    break;
+                case 16:
+                case 17:
+                    USART3.begin(115200);
+                    busart3EN = true;
+                    debug.println("LIN3 Enabled");
+                    break;
+                case 20:
+                case 21:
+                    USART4.begin(115200);
+                    busart4EN = true;
+                    debug.println("LIN4 Enabled");
+                    break;
+                case 24:
+                case 25:
+                    //USART0.begin(115200);
+                    //usart0EN = true;
+                    debug.println("Error USART5 Is USED for I2C");
+                    break;
+                case 28:
+                case 29:
+                    USART6.begin(115200);
+                    busart6EN = true;
+                    debug.println("LIN6 Enabled");
+                    break;
+                case 34:
+                case 35:
+                    USART7.begin(115200);
+                    busart7EN = true;
+                    debug.println("LIN7 Enabled");
+                    break;  
+            }
+            break;
+        case _SPI:
+            debug.printf("Pin: %d is Set as SPI\n",x);
+            break;
+        case _I2C:
+            switch (x)
+            {
+            case 18:
+            case 19:
+                bi2c0EN = true;
+                debug.printf("Pin: %d is Set as I2C0\n",x);
+                break;
+            case 16:
+            case 17:
+                bi2c1EN = true;
+                debug.printf("Pin: %d is Set as I2C1\n",x);
+                break;
+            case 24:
+            case 25:
+                bi2c2EN = true;
+                debug.printf("Pin: %d is Set as I2C2\n",x);
+                break;
+            default:
+                break;
+            }
+            debug.printf("Pin: %d is Set as I2C\n",x);
+            break;
+        case _ADC:
+            analog[adcCount].Setup(x,adcCount);
+            adcCount++;
+            debug.printf("Pin: %d is Set as ADC\n",x);
+            break;
+        case _PWM:
+            pwm[pwmCount].Setup(x,x+PWM0ID);
+            pwmCount++;
+            debug.printf("Pin: %d is Set as PWM\n",x);
+            break;
+        default:
+            debug.printf("Pin: %d is Unknown\n",x);
+            break;
+        }
+    }
+    debug.printf("There are %d digital out pins\n",gpoCount);
+    debug.printf("There are %d digital in pins\n",gpiCount);
+    debug.printf("There are %d analog in pins\n",adcCount);
+    debug.printf("There are %d pwm pins\n",pwmCount);
+
+}
+
+bool usart0EN(void)
+{
+    return busart0EN;
+}
+bool usart1EN(void)
+{
+    return busart1EN;
+}
+bool usart2EN(void)
+{
+    return busart2EN;
+}
+bool usart3EN(void)
+{
+    return busart3EN;
+}
+bool usart4EN(void)
+{
+    return busart4EN;
+}
+bool usart5EN(void)
+{
+    return busart5EN;
+}
+bool usart6EN(void)
+{
+    return busart6EN;
+}
+bool usart7EN(void)
+{
+    return busart7EN;
+}
+
+
+bool can0EN(void)
+{
+    return bcan0EN;
+}
+bool can1EN(void)
+{
+    return bcan1EN;
+}
+bool can2EN(void)
+{
+    return bcan2EN;
+}
+
+bool led0EN(void)
+{
+    return bled0EN;
+}
+
+bool led1EN(void)
+{
+    return bled1EN;
+}
+
+bool led2EN(void)
+{
+    return bled2EN;
+}
+
+bool led3EN(void)
+{
+    return bled3EN;
+}
+
+bool i2c0EN(void)
+{
+    return bi2c0EN;
+}
+
+bool i2c1EN(void)
+{
+    return bi2c1EN;
+}
+
+bool i2c2EN(void)
+{
+    return bi2c2EN;
+}
---
+++ pin_mapping_T4.h
@@ -0,0 +1,154 @@
+#ifndef _PIN_SETTING_H_
+#define _PIN_SETTING_H_
+
+#include <Arduino.h>
+
+
+
+#define LED0ID      0X2A
+#define LED1ID      0X2B
+#define LED2ID      0X2C
+#define LED3ID      0X2D
+
+#define GPIORID     0X2E
+#define GPIOWID     0X2F
+
+#define VERSIONID   0X30
+
+#define LCDID       0X31 
+
+#define DEBUGID     0X3D
+
+#define RESETID     0X3E
+
+#define MEMORYID    0X3F
+
+#define CAN0ID      0X40
+#define CAN1ID      0X41
+#define CAN2ID      0X42
+#define CANFDID     0X43
+
+#define LINID       0X44
+
+#define SPI0ID      0X45
+#define SPI1ID      0X46
+
+#define USART0ID    0X47
+#define USART1ID    0X48
+#define USART2ID    0X49
+#define USART3ID    0X4A
+#define USART4ID    0X4B
+#define USART5ID    0X4C
+#define USART6ID    0X4D
+#define USART7ID    0X4E
+
+#define I2C0ID      0X4F
+#define I2C1ID      0X50 
+#define I2C2ID      0X51 
+
+#define ETHERID     0X52
+
+#define ADC0ID      0X53
+#define ADC1ID      0X54
+#define ADC2ID      0X55
+#define ADC3ID      0X56
+#define ADC4ID      0X57
+#define ADC5ID      0X58
+#define ADC6ID      0X59
+#define ADC7ID      0X5A
+#define ADC8ID      0X5B
+#define ADC9ID      0X5C
+#define ADCAID      0X5D
+#define ADCBID      0X5E
+#define ADCCID      0X5F
+#define ADCDID      0X60
+#define ADCEID      0X61
+#define ADCFID      0X62
+
+#define PWM0ID      0X63 
+#define PWM1ID      0X64
+#define PWM2ID      0X65 
+#define PWM3ID      0X66 
+#define PWM4ID      0X67 
+#define PWM5ID      0X68 
+#define PWM6ID      0X69 
+#define PWM7ID      0X6A 
+#define PWM8ID      0X6B 
+#define PWM9ID      0X6C 
+#define PWMAID      0X6D 
+#define PWMBID      0X6E
+#define PWMCID      0X6F 
+#define PWMDID      0X70 
+#define PWMEID      0X71 
+#define PWMFID      0X72 
+#define PWM10ID     0X73 
+#define PWM11ID     0X74 
+#define PWM12ID     0X75
+#define PWM13ID     0X76 
+#define PWM14ID     0X77 
+#define PWM15ID     0X78 
+#define PWM16ID     0X79 
+#define PWM17ID     0X7A 
+#define PWM18ID     0X7B 
+#define PWM19ID     0X7C 
+#define PWM1AID     0X7D 
+#define PWM1BID     0X7E 
+
+#define HEARTBEAT   0X7F 
+
+
+
+
+
+#define _GPO        0x00
+#define _GPI        0x01
+#define _CAN        0x08 
+#define _USART      0x0A
+#define _LIN        0X0B 
+#define _SPI        0x0C
+#define _I2C        0x0E 
+#define _ADC        0x10
+#define _PWM        0x20
+#define _LEDM0      0X40
+#define _LEDM1      0X52
+
+
+
+
+bool can0EN(void);
+bool can1EN(void);
+bool can2EN(void);
+
+static uint8_t gpoCount;
+static uint8_t gpiCount;
+static uint8_t gpioCount;
+static uint8_t adcCount;
+static uint8_t pwmCount;
+
+
+//static uint8_t gpo[40];
+//static uint8_t gpi[40];
+//static uint8_t adc_pin[16];
+//static uint8_t pwm[29];
+
+
+void parsePins(u_int8_t *pinCon, int pinCount);
+void parsePins(u_int8_t *pinCon);
+bool usart0EN(void);
+bool usart1EN(void);
+bool usart2EN(void);
+bool usart3EN(void);
+bool usart4EN(void);
+bool usart5EN(void);
+bool usart6EN(void);
+bool usart7EN(void);
+
+bool led0EN(void);
+bool led1EN(void);
+bool led2EN(void);
+bool led3EN(void);
+
+bool i2c0EN(void);
+bool i2c1EN(void);
+bool i2c2EN(void);
+#endif
